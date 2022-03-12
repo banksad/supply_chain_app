896feddcabb25ea29b53d796406649aba97667e9
Andy Banks <andrewdhbanks@gmail.com>
2022-03-12 23:11:20 +0000

    Add markdown text and remove progress bar

--- streamlit-app.py
+++ streamlit-app.py
@@ -6,13 +6,17 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 
-st.progress(progress_variable_1_to_100)
-
 st.set_page_config(layout="wide")
 
 st.title('Supply Chain Analysis')
 
 st.subheader('Streamlit App by [Andy Banks](https://github.com/banksad)')
+
+st.markdown("""This is a prototype dashboard to present a range of publicly available information on supply chains.
+            
+            You can find the source code [here](https://github.com/banksad/supply_chain_app)
+            
+            Feel free to do a pull request :smile:""")
 
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
