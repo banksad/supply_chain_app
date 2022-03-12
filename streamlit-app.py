4e0608a280921e3cc53906ab852fe5b40167f36f
Andy Banks <andrewdhbanks@gmail.com>
2022-03-12 22:30:32 +0000

    Add title
    Signed-off-by: Andy Banks <andrewdhbanks@gmail.com>

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,6 +8,8 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
+st.title('Supply Chain Analysis')
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
