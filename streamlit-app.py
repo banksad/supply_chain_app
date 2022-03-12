11be10d6520be6a3aa1312a2b149e919d066c616
Andy Banks <andrewdhbanks@gmail.com>
2022-03-12 23:24:36 +0000

    Add data

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,15 +8,17 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
+# Data import
+country_imports = pd.read_csv("./data/UNSDfinal.csv")
+
+# Page design
 st.title('Supply Chain Analysis')
 
 st.subheader('Streamlit App by [Andy Banks](https://github.com/banksad)')
 
 st.markdown('This is a prototype dashboard to present a range of publicly available information on supply chains.')
             
-st.markdown('You can find the source code [here](https://github.com/banksad/supply_chain_app)')
-            
-st.markdown('Feel free to do a pull request :smile:')
+st.markdown('You can find the source code [here](https://github.com/banksad/supply_chain_app). Feel free to do a pull request :smile:')
 
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
