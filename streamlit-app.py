a23d9a2b576cf242f42d051cff6c107c1294769c
Andy Banks <andrewdhbanks@gmail.com>
2022-03-13 00:53:14 +0000

    Add seaborn chart

--- streamlit-app.py
+++ streamlit-app.py
@@ -6,7 +6,7 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import pandas as pd
 import altair as alt
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 
 st.set_page_config(layout="wide")
