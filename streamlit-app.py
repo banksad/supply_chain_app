2d0fcf597ef701ae4c8d40a3393531605ca87f5e
Andy Banks <andrewdhbanks@gmail.com>
2022-03-14 12:03:12 +0000

    Add fig

--- streamlit-app.py
+++ streamlit-app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import networkx as nx
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 st.set_page_config(layout="wide")
