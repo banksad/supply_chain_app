66435ed7911af1a67b6c572fcfeff92e6e8921c0
Andy Banks <andrewdhbanks@gmail.com>
2022-04-06 15:34:21 +0100

    Add pie chart

--- streamlit-app.py
+++ streamlit-app.py
@@ -7,6 +7,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.express as px
 
 st.set_page_config(layout="centered")
 
@@ -27,5 +28,10 @@ st.subheader('Import content of domestically produced products')
 
 st.markdown("**Select level of detail you want to analyze:** 👇")
 
-detail = st.multiselect('Pick a product',set(list(imports_use['output product'])))
+detail = st.multiselect('Search for a product',set(list(imports_use['output product'])))
 
+import_subset = imports_use[imports_use['output product']==detail]
+import_subset['proportion'] = import_subset['value'] / import_subset['value'].sum()
+
+fig = px.pie(import_subset, values='proportion', names='import requirements')
+st.plotly_chart(fig, use_container_width=True)
