cad2cf8a1f699e48d39d7410b9345d226245819c
Andy Banks <andrewdhbanks@gmail.com>
2022-04-08 00:32:08 +0100

    negative values

--- streamlit-app.py
+++ streamlit-app.py
@@ -124,8 +124,18 @@ total_coe = (effects_subset[(effects_subset['factor']=='total impact')&(effects_
 
 # Writing
 
-st.write('The change in gross value added is £', round(total_gva, 2), 'm')
+if change<0:
+    
+    st.write('The change in gross value added is -£', round(total_gva, 2), 'm')
 
-st.write('This is because there is a corresponding change to imports of £', round(total_imports,2), 'm')
+    st.write('This is because there is a corresponding change to imports of -£', round(total_imports,2), 'm')
 
-st.write('Employee compensation would increase by', round(total_coe,2), 'm')
+    st.write('Employee compensation would increase by -£', round(total_coe,2), 'm')
+    
+else:
+
+    st.write('The change in gross value added is £', round(total_gva, 2), 'm')
+
+    st.write('This is because there is a corresponding change to imports of £', round(total_imports,2), 'm')
+
+    st.write('Employee compensation would increase by £', round(total_coe,2), 'm')
