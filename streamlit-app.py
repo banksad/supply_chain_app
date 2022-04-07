84044b81615f0b70d37eef4adf94ede75bdbf73d
Andy Banks <andrewdhbanks@gmail.com>
2022-04-07 21:54:00 +0100

    Amendments

--- streamlit-app.py
+++ streamlit-app.py
@@ -56,20 +56,20 @@ st.markdown('This section examines both domestically produced and imported produ
 combined_subset = combined[combined['output product']==combined_product]
 combined_subset = combined_subset[combined_subset['proportion']>0]
         
-st.markdown('##### Inputs used in the domestic production of {} products'.format(combined_product.lower()))
-
 st.markdown('Choose whether to view total inputs, or a breakdown of domestically produced and imported inputs')
 
 chart_choice = st.selectbox('',['Total inputs','Domestic / Imported breakdown'])
 
 if chart_choice == 'Domestic / Imported breakdown':
     
+    st.markdown('##### Domestically produced and imported inputs used in the domestic production of {} products'.format(combined_product.lower()))    
     fig = px.treemap(combined_subset, path=['component','input product'], values='value',
                     color='value',color_continuous_scale='OrRd')
     st.plotly_chart(fig, use_container_width=True)
 
 else:
     
+    st.markdown('##### Total inputs used in the domestic production of {} products'.format(combined_product.lower()))
     fig = px.treemap(combined_subset, path=['input product'], values='value',
                     color='value',color_continuous_scale='OrRd')
     st.plotly_chart(fig, use_container_width=True)
