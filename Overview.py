f615d9ea19c6a74af2c45416a60c53e15a3f74c6
Andy Banks <andrewdhbanks@gmail.com>
2022-07-19 22:53:16 +0100

    Changed floating point

--- Overview.py
+++ Overview.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.markdown("# Main page")
-st.sidebar.markdown("# Main page")
+st.markdown("# Overview")
 
 # Sidebar
 
 st.markdown('This is a prototype dashboard to present a range of publicly available information on supply chains.')
+st.markdown('Different pages can be clicked on in the sidebar. These each contain interactive content relating to the Input Output tables produced by the Office for National Statistics (ONS)')
 st.markdown('The text and charts automatically update depending on the options chosen in the selection boxes.')
 st.markdown('You can find the source code [here](https://github.com/banksad/supply_chain_app)')
