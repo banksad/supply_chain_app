db9443f01f365a59152282976e2b5c8e001d3122
Andy Banks <andrewdhbanks@gmail.com>
2022-08-15 15:27:05 +0100

    add chart and extra text

--- pages/02_Effect_of_a_demand_change.py
+++ pages/02_Effect_of_a_demand_change.py
@@ -18,9 +18,10 @@ multiplier_product = st.selectbox('Select a product',options=detailed_effects.so
 
 change = st.number_input('Input change in demand for the product (£m)')
 
-# Calculations
+# Total calculations
 
 effects_subset = detailed_effects[detailed_effects['product']==multiplier_product]
+total_effects = effects_subset[effects_subset['factor']=='total impact']
 
 total_gva = (effects_subset[(effects_subset['factor']=='total impact')&(effects_subset['variable']=='Gross value added')]['value']*change).values[0]
 
@@ -28,20 +29,45 @@ total_imports = (effects_subset[(effects_subset['factor']=='total impact')&(effe
 
 total_coe = (effects_subset[(effects_subset['factor']=='total impact')&(effects_subset['variable']=='Compensation of employees')]['value']*change).values[0]
 
+total_go = (effects_subset[(effects_subset['factor']=='total impact')&(effects_subset['variable']=='Output')]['value']*change).values[0]
+
+total_effects['total_effects'] = total_effects['value'] * change
+
 # Writing
 
 if change<0:
     
     st.write('The change in gross value added is -£{:,}m'.format(round(total_gva*-1, 2)))
 
-    st.write('This is because there is a corresponding change to imports of -£{:,}m', round(total_imports*-1,2))
+    st.write('This is because domestic output changes by -£{:,}m, and there is a change in imports of -£{:,}m'.format(round(total_imports*-1,2),round(total_imports*-1,2)))
 
     st.write('Employee compensation would change by -£{:,}m', round(total_coe*-1,2))
+
+    # Add chart
+
+    st.markdown('##### Total effects of a change to final use of -£{:,}m for {}'.format(round(change*-1,2),multiplier_product.lower()))    
+    fig = px.bar(total_effects, x='total_effects', y='factor'
+                    labels={
+                        'total_effects':'Total effect',
+                        'factor': 'Component'
+                    })
+    st.plotly_chart(fig, use_container_width=True)
     
 else:
 
     st.write('The change in gross value added is £{:,}m'.format(round(total_gva, 2)))
 
-    st.write('This is because there is a corresponding change to imports of £{:,}m'.format(round(total_imports,2)))
+    st.write('This is because domestic output changes by £{:,}m, and there is a change in imports of £{:,}m'.format(round(total_imports,2),round(total_imports,2)))
 
     st.write('Employee compensation would change by £{:,}m'.format(round(total_coe,2)))
+
+    # Add chart
+
+    st.markdown('##### Total effects of a change to final use of £{:,}m for {}'.format(round(change*-1,2),multiplier_product.lower()))    
+    fig = px.bar(total_effects, x='total_effects', y='factor'
+                    labels={
+                        'total_effects':'Total effect',
+                        'factor': 'Component'
+                    })
+    st.plotly_chart(fig, use_container_width=True)
+
