9c08a33e146ef4e8a8d668cd7f74d548d0ecf565
Andy Banks <andrewdhbanks@gmail.com>
2022-08-26 16:59:59 +0100

    table

--- pages/02_Inputs_into_the_production_process.py
+++ pages/02_Inputs_into_the_production_process.py
@@ -107,6 +107,9 @@ else:
         st.plotly_chart(fig, use_container_width=True, config=config)
         
 see_import_data3 = st.expander('You can click here to see the raw data. Data are currently ordered in descending order of the proportion of inputs used in the production of a product.')
+
+data_viewer = combined_subset.rename(columns={'component':'Component','input product':'Input Product','value':'Value (£m)','proportion':'Proportion'})
+
 with see_import_data3:
 
         # CSS to inject contained in a string
@@ -120,4 +123,4 @@ with see_import_data3:
     # Inject CSS with Markdown
     st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
-    st.table(data=combined_subset[['component','input product','value','proportion']].sort_values(by='value',ascending=False))
+    st.table(data=data_viewer[['component','input product','value','proportion']].sort_values(by='value',ascending=False))
