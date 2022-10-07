a8789fa3ab7239e27d33f6a2d192e50ab965a4c4
banksad <andrewdhbanks@gmail.com>
2022-10-07 15:04:10 +0100

    restrict to 10

--- pages/01_Industry_use_of_goods_and_services.py
+++ pages/01_Industry_use_of_goods_and_services.py
@@ -27,7 +27,7 @@ def truncate(x):
 
     return x[:20]+'...'
 
-combined_subset['industry_trun'] = combined_subset['industry'].apply(lambda x: truncate(x)) 
+combined_subset['industry_trun'] = combined_subset['industry'].apply(lambda x: truncate(x)).sort_values(by='proportion',ascending='False').head(10)
 
 # Chart choice
 
