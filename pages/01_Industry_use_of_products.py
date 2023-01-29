264f33b8ab50df14039f3ec11b2475cf26ed70a7
banksad <andrewdhbanks@gmail.com>
2023-01-29 15:24:44 +0000

    fig1

--- pages/01_Industry_use_of_products.py
+++ pages/01_Industry_use_of_products.py
@@ -86,19 +86,7 @@ else:
     fig = px.histogram(combined_subset, 
                         color='component',
                         y='industry_trun', 
-                        x='proportion_total',
-                            labels={
-                                'component':'Category',
-                                'industry_trun':'Industry',
-                                'proportion_total': 'percentage of total inputs'
-                            },
-            height=600,
-            hover_name='industry',
-            hover_data={
-                        'proportion_total':':.1f',
-                        'component':False,
-                        'industry_trun':False
-                        })
+                        x='proportion_total')
 
     fig.update_layout(barmode='stack',
                       xaxis_title='Percentage of total intermediate consumption',
